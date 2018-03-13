90341e9f2ecb74a0388ca5efd294cfc231249cba
Mikkel Flindt Heisterberg <mheisterberg@salesforce.com>
2018-03-13 20:58:19 +0100

    add postgres

--- src/index.js
+++ src/index.js
@@ -1,10 +1,20 @@
 const express = require('express')
 const bodyparser = require('body-parser')
+const terminateListener = require('./terminate-listener.js');
+const pg = require('pg');
+const connectionString = process.env.DATABASE_URL || 'postgres://localhost:5432/todo';
+
+// connect to db
+const client = new pg.Client(connectionString);
+client.connect();
 
 const app = express()
 app.use(bodyparser.json())
 
 app.post('/*', (req, res) => {
+  req.body.forEach(element => {
+    client.query(`insert into sensor_data (dt, id, value) values (current_timestamp, '${element.sensorId}', ${element.sensorValue});`);
+  });
   let j = JSON.stringify(req.body, undefined, 2)
   console.log(`Received: ${j}`)
   res.setHeader('Content-Type', 'text/plain')
@@ -16,3 +26,10 @@ app.get('/*', (req, res) => {
 })
 
 app.listen(process.env.PORT || 8080)
+
+// setup termination listener
+terminateListener(() => {
+  logger.info("Closing postgres driver");
+  client.end();
+  logger.info("Closed postgres driver");
+});
