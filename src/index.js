5e9952556a678e88b9e63598e4e9b22f253a31df
Mikkel Flindt Heisterberg <mheisterberg@salesforce.com>
2018-03-14 12:10:28 +0100

    Return data

--- src/index.js
+++ src/index.js
@@ -12,24 +12,55 @@ const app = express()
 app.use(bodyparser.json())
 
 app.post('/*', (req, res) => {
-  req.body.forEach(element => {
-    client.query(`insert into sensor_data (dt, id, value) values (current_timestamp, '${element.sensorId}', ${element.sensorValue});`);
-  });
-  let j = JSON.stringify(req.body, undefined, 2)
-  console.log(`Received: ${j}`)
-  res.setHeader('Content-Type', 'text/plain')
-  res.send(`Thank you - you posted: ${j}\n`).end()
+  pg.connect(connectionString, (err, client, done) => {
+    // handle connection errors
+    if (err) {
+      done();
+      console.log(err);
+      return res.status(500).json({success: false, data: err});
+    }
+
+    req.body.forEach(element => {
+      client.query(`insert into sensor_data (dt, id, value) values (current_timestamp, '${element.sensorId}', ${element.sensorValue});`);
+    });
+    done();
+
+    let j = JSON.stringify(req.body, undefined, 2)
+    console.log(`Received: ${j}`)
+    res.setHeader('Content-Type', 'text/plain')
+    res.send(`Thank you - you posted: ${j}\n`).end()
+  })
 })
 app.get('/*', (req, res) => {
-  res.setHeader('Content-Type', 'text/plain')
-  res.send('HelloWorld...').end()
+  const minutes = req.param.minutes || 240
+  
+  pg.connect(connectionString, (err, client, done) => {
+    // handle connection errors
+    if (err) {
+      done();
+      console.log(err);
+      return res.status(500).json({success: false, data: err});
+    }
+
+    const results = []
+    const query = client.query(`select s.id id, s.name sensor_name, to_char(d.dt, 'YYYY-MM-DD HH24:MI:SS') dt, d.value from sensor_data d, sensor s where d.id=s.id and current_timestamp - dt < interval '${minutes} minutes' order by id asc, dt asc`)
+    query.on('row', (row) => {
+      results.push(row);
+    });
+
+    // all data is returned, close connection and return results
+    query.on('end', () => {
+      done();
+      return res.json(results);
+    });
+  })
 })
 
 app.listen(process.env.PORT || 8080)
 
 // setup termination listener
 terminateListener(() => {
-  logger.info("Closing postgres driver");
-  client.end();
-  logger.info("Closed postgres driver");
+  console.log("Closing postgres driver");
+  pg.end();
+  console.log("Closed postgres driver");
 });
