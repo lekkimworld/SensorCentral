6f8e322d7908fbe7b92f8ef75ea869fced654af0
Mikkel Flindt Heisterberg <mheisterberg@salesforce.com>
2019-01-08 21:00:50 +0100

    Duh

--- src/services/notify-service.js
+++ src/services/notify-service.js
@@ -12,7 +12,7 @@ NotifyService.prototype.init = function(callback, logSvc, eventSvc, pushoverSvc)
     let pushoverLastSent = undefined
 
     eventSvc.subscribe([constants.PUBNUB.CTRL_CHANNEL, constants.PUBNUB.RAW_SENSORREADING_CHANNEL], (channel, obj) => {
-        logSvc.debug(`Notify service received message on channel ${channel} with payload title=${title} and msg=${obj}`)
+        logSvc.debug(`Notify service received message on channel ${channel} with payload=${obj}`)
         if (channel === constants.PUBNUB.CTRL_CHANNEL) {
             if (obj.hasOwnProperty('restart') && true === obj.restart) {
                 // this is restart event - notify
