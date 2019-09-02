d52ed8468a78f00cbd217ae7667fbe7b38572d29
Mikkel Flindt Heisterberg <mheisterberg@salesforce.com>
2019-09-02 08:38:31 +0200

    Readd Pushover support back in

--- src/services/pushover-service.ts
+++ src/services/pushover-service.ts
@@ -1,25 +1,14 @@
-const {Pushover} = require("node-pushover");
+const Pushover = require("node-pushover");
 import { BaseService } from "../types";
 import { LogService } from "./log-service";
 
 // get pushover data from env
 const PUSHOVER_APPTOKEN = process.env.PUSHOVER_APPTOKEN
 const PUSHOVER_USERKEY = process.env.PUSHOVER_USERKEY
-const pushover = (function() {
-    if (PUSHOVER_USERKEY && PUSHOVER_APPTOKEN) {
-        return new Pushover({
-            token: PUSHOVER_APPTOKEN,
-            user: PUSHOVER_USERKEY
-        })
-    }
-})()
-if (!pushover) {
-    // nothing to do here
-    console.log('Pushover support not configured...')
-}
 
 export class PushoverService extends BaseService {
     logService? : LogService;
+    pushover : any;
 
     constructor() {
         super("pushover");
@@ -27,11 +16,23 @@ export class PushoverService extends BaseService {
     }
     init(callback : (err? : Error) => {}, services : BaseService[]) {
         this.logService = services[0] as unknown as LogService;
+
+        if (PUSHOVER_USERKEY && PUSHOVER_APPTOKEN) {
+            this.logService!.info(`PUSHOVER_USERKEY and PUSHOVER_APPTOKEN found in environment - creating Pushover support`);
+            this.pushover = new Pushover({
+                token: PUSHOVER_APPTOKEN,
+                user: PUSHOVER_USERKEY
+            });
+        } else {
+            this.logService!.info(`No PUSHOVER_USERKEY and PUSHOVER_APPTOKEN found in environment - NO Pushover support`);
+        }
+
+        // callback
         callback();
     }
     notify(title : string, msg : string) : void {
-        if (!pushover) return
+        if (!this.pushover) return
         this.logService!.debug(`Asked to notify with payload title=${title} and msg=${msg}`)
-        pushover.send(title, msg)
+        this.pushover.send(title, msg)
     }
 }
