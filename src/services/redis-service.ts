38e250bc36397417272eb391478ae6cb17349bb2
Mikkel Flindt Heisterberg <mheisterberg@salesforce.com>
2020-12-14 14:29:25 +0100

    Update redis connection method

--- src/services/redis-service.ts
+++ src/services/redis-service.ts
@@ -1,16 +1,35 @@
 import {promisify} from "util";
 import {BaseService} from "../types";
-import * as redis from "redis";
+import url from "url";
+import {createClient as createRedisClient} from "redis";
 
 const CONNECTION_TIMEOUT = 
     process.env.REDIS_CONNECTION_TIMEOUT ? 
     Number.parseInt(process.env.REDIS_CONNECTION_TIMEOUT) : 
     5000;
 
-const client = redis.createClient({
-    "url": process.env.REDIS_URL,
-    "connect_timeout": CONNECTION_TIMEOUT
-} as redis.ClientOpts);
+const client = (function() {
+    const redis_uri = process.env.REDIS_URL ? url.parse(process.env.REDIS_URL as string) : undefined;
+    if (process.env.REDIS_URL && redis_uri && redis_uri.protocol!.indexOf("rediss") === 0) {
+        return createRedisClient({
+            port: Number.parseInt(redis_uri.port!),
+            host: redis_uri.hostname!,
+            password: redis_uri.auth!.split(':')[1],
+            db: 0,
+            tls: {
+                rejectUnauthorized: false,
+                requestCert: true,
+                agent: false
+            },
+            connect_timeout: CONNECTION_TIMEOUT
+        })
+     } else {
+         return createRedisClient({
+             "url": process.env.REDIS_URL as string,
+             "connect_timeout": CONNECTION_TIMEOUT
+         });
+     }
+})();
 
 const promisifiedClient = {
     'get': promisify(client.get).bind(client),
