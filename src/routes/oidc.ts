50ddf5faa1f1691466d0b7bfb905ed17aff288f0
Mikkel Flindt Heisterberg <mheisterberg@salesforce.com>
2021-02-12 09:25:45 +0100

    Add logging

--- src/routes/oidc.ts
+++ src/routes/oidc.ts
@@ -1,11 +1,12 @@
 import express from "express";
-import {getOidcClient} from "../oidc-authentication-utils";
-import { BrowserLoginResponse, HttpException, LoginSource } from "../types";
+import { getOidcClient } from "../oidc-authentication-utils";
+import { HttpException, LoginSource } from "../types";
 import { CreateLoginUserInput } from "../services/identity-service";
 //@ts-ignore
 import { lookupService } from "../configure-services";
 import { buildBaseHandlebarsContext } from "../utils";
 import { IdentityService } from "../services/identity-service";
+import { LogService } from "../services/log-service";
 
 // create a router
 const router = express.Router();
@@ -18,9 +19,16 @@ router.get("/callback", async (req, res, next) => {
     const session = req.session as any;
     const nonce = session!.nonce;
     if (!nonce) return next(new HttpException(417, `No nonce found (<${nonce}>)`));
-    
+
+    // get services
+    const svcs = await lookupService([IdentityService.NAME, LogService.NAME]);
+    const identService = svcs[0] as IdentityService;
+    const logService = svcs[1] as LogService;
+
     // get client
+    logService.debug("Retrieving OIDC client");
     const oidcClient = await getOidcClient();
+    logService.debug("Retrieve OIDC client");
 
     // get params
     const callbackParams = oidcClient.callbackParams(req);
@@ -29,38 +37,44 @@ router.get("/callback", async (req, res, next) => {
             "client_secret": process.env.OIDC_CLIENT_SECRET
         }
     } : {};
-    oidcClient.callback(process.env.OIDC_REDIRECT_URI, callbackParams, { nonce }, callbackExtras).then((tokenSet) => {
+
+    try {
+        // get tokenset
+        const tokenSet = await oidcClient.callback(process.env.OIDC_REDIRECT_URI, callbackParams, { nonce }, callbackExtras);
+        logService.debug("Performed OIDC callback and retrieved tokenset");
+
         // get claims and validate hosted domain
         const claims = tokenSet.claims();
         if (process.env.GOOGLE_HOSTED_DOMAIN && (!claims.hd || claims.hd !== process.env.GOOGLE_HOSTED_DOMAIN)) {
             return next(new HttpException(417, "Unable to validate hosted domain claim"));
         }
-        
+        logService.debug(`Retrieved OIDC claims (${JSON.stringify(claims)})`);
+
         // ensure we have a row in LOGIN_USER for the user
-        lookupService(IdentityService.NAME).then((identity : IdentityService) => {
-            return identity.getOrCreateBrowserLoginResponse({
-                source: LoginSource.google, 
-                oidc_sub: claims.sub as string, 
-                email: claims.email as string,
-                ln: claims.family_name,
-                fn: claims.given_name
-            } as CreateLoginUserInput);
+        const output = await identService.getOrCreateBrowserLoginResponse({
+            source: LoginSource.google,
+            oidc_sub: claims.sub as string,
+            email: claims.email as string,
+            ln: claims.family_name,
+            fn: claims.given_name
+        } as CreateLoginUserInput);
+        logService.debug(`Created BrowserLoginResponse`);
 
-        }).then((output : BrowserLoginResponse) => {
-            // set the claims we received, set userId in session and redirect
-            const session = req.session as any;
-            session!.browserResponse = output;
+        // set the claims we received, set userId in session and redirect
+        session!.browserResponse = output;
 
-            // redirect
-            res.redirect("/openid/loggedin");
-        })
-    });
+        // redirect
+        res.redirect("/openid/loggedin");
+
+    } catch (err) {
+        return next(new HttpException(417, `Unable to perform callback (${err}`, err))
+    }
 })
 
 /**
  * After logging in the user is redirected to this URL.
  */
-router.get("/loggedin", ({res}) => {
+router.get("/loggedin", ({ res }) => {
     return res!.render("loggedin", Object.assign({}, buildBaseHandlebarsContext()));
 })
 
