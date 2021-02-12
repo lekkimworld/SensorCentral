6ab7baff353cb5e915e7579ec316f983b8743cf5
Mikkel Flindt Heisterberg <mheisterberg@salesforce.com>
2021-02-12 10:11:23 +0100

    Implement init method for redis service

--- src/services/redis-service.ts
+++ src/services/redis-service.ts
@@ -1,14 +1,15 @@
-import {promisify} from "util";
-import {BaseService} from "../types";
+import { promisify } from "util";
+import { BaseService } from "../types";
 import url from "url";
-import {createClient as createRedisClient} from "redis";
+import { createClient as createRedisClient } from "redis";
+import { LogService } from "./log-service";
 
-const CONNECTION_TIMEOUT = 
-    process.env.REDIS_CONNECTION_TIMEOUT ? 
-    Number.parseInt(process.env.REDIS_CONNECTION_TIMEOUT) : 
-    20000;
+const CONNECTION_TIMEOUT =
+    process.env.REDIS_CONNECTION_TIMEOUT ?
+        Number.parseInt(process.env.REDIS_CONNECTION_TIMEOUT) :
+        20000;
 
-const client = (function() {
+const client = (function () {
     const redis_uri = process.env.REDIS_URL ? url.parse(process.env.REDIS_URL as string) : undefined;
     if (process.env.REDIS_URL && redis_uri && redis_uri.protocol!.indexOf("rediss") === 0) {
         return createRedisClient({
@@ -23,12 +24,12 @@ const client = (function() {
             },
             connect_timeout: CONNECTION_TIMEOUT
         })
-     } else {
-         return createRedisClient({
-             "url": process.env.REDIS_URL as string,
-             "connect_timeout": CONNECTION_TIMEOUT
-         });
-     }
+    } else {
+        return createRedisClient({
+            "url": process.env.REDIS_URL as string,
+            "connect_timeout": CONNECTION_TIMEOUT
+        });
+    }
 })();
 
 const promisifiedClient = {
@@ -43,11 +44,21 @@ const promisifiedClient = {
 
 export class RedisService extends BaseService {
     public static NAME = "redis";
+    dependencies = [LogService.NAME];
 
     constructor() {
         super(RedisService.NAME);
     }
-    
+
+    init(callback: (err?: Error) => {}, services: BaseService[]) {
+        const log = services[0] as LogService;
+        log.info("Querying redis for dummy key...");
+        client.get("foo", (err, data) => {
+            log.info("Queried redis for dummy key - result: " + data + ", err: " + err);
+            callback(err || undefined);
+        })
+    }
+
     terminate() {
         return new Promise<void>((resolve) => {
             client.end();
@@ -55,30 +66,30 @@ export class RedisService extends BaseService {
         });
     }
 
-    del(key:string) : Promise<string> {
+    del(key: string): Promise<string> {
         return promisifiedClient.del(key);
     }
-    get(key:string) : Promise<string> {
+    get(key: string): Promise<string> {
         return promisifiedClient.get(key);
     }
-    set(key:string, value:string) : Promise<void> {
+    set(key: string, value: string): Promise<void> {
         return promisifiedClient.set(key, value) as Promise<void>;
     }
-    setex(key:string, expire:number, value:string) : Promise<string> {
+    setex(key: string, expire: number, value: string): Promise<string> {
         return promisifiedClient.setex(key, expire, value);
     }
-    keys(pattern:string) : Promise<string[]> {
+    keys(pattern: string): Promise<string[]> {
         return promisifiedClient.keys(pattern);
     }
-    mget(...keys:string[]) : Promise<string[]> {
+    mget(...keys: string[]): Promise<string[]> {
         return new Promise((resolve, reject) => {
-            client.mget(keys, (err : Error | null, values : string[]) => {
+            client.mget(keys, (err: Error | null, values: string[]) => {
                 if (err) return reject(err);
                 resolve(values);
             });
         });
     }
-    expire(key:string, expire:number) : Promise<number> {
+    expire(key: string, expire: number): Promise<number> {
         return promisifiedClient.expire(key, expire);
     }
 
