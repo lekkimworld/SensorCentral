99afab49c39a6ac730d0e2f9c9d23dde4b748ecd
Mikkel Flindt Heisterberg <lekkim@heisterberg.dk>
2021-11-12 12:06:59 +0100

    Enfore TLS

--- src/configure-express-session.ts
+++ src/configure-express-session.ts
@@ -1,10 +1,10 @@
-import session from 'express-session';
-import RedisStore from 'connect-redis';
+import session, { SessionOptions } from "express-session";
+import RedisStore from "connect-redis";
 import constants from "./constants";
 import { RedisClient } from "redis";
-import {v4 as uuid} from "uuid";
+import { v4 as uuid } from "uuid";
 
-const secret = (function() {
+const secret = (function () {
     if (process.env.SESSION_SECRET) {
         console.log(`Using SESSION_SECRET provided by environment`);
         return process.env.SESSION_SECRET;
@@ -15,15 +15,30 @@ const secret = (function() {
 })();
 console.log(`Read session timeout in sesconds as <${constants.DEFAULTS.SESSION_TIMEOUT_SECONDS}>`);
 const redisStoreInstance = RedisStore(session);
-export default (redisClient : RedisClient) => {
-    return session({
-        "saveUninitialized": false,
-        "resave": false,
-        "secret": secret,
-        "store": new redisStoreInstance({
-            "client": redisClient,
-            "prefix": "session:",
-            "ttl": constants.DEFAULTS.SESSION_TIMEOUT_SECONDS
-        })
-    })
-}
+
+export default (redisClient: RedisClient) => {
+    // create session options
+    const sessionOptions: SessionOptions = {
+        saveUninitialized: false,
+        resave: false,
+        secret: secret,
+        store: new redisStoreInstance({
+            client: redisClient,
+            prefix: "session:",
+            ttl: constants.DEFAULTS.SESSION_TIMEOUT_SECONDS,
+        }),
+    };
+
+    // if not development ensure secure
+    if (process.env.NODE_ENV !== "development") {
+        console.log("NODE_ENV not set to 'development' - enforcing cookie settings");
+        sessionOptions.cookie = {
+            sameSite: "strict",
+            secure: true,
+            httpOnly: true,
+        };
+    }
+
+    // create sessions
+    return session(sessionOptions);
+};
--- src/configure-express.ts
+++ src/configure-express.ts
@@ -1,27 +1,41 @@
-import express from 'express';
-import {json as bp_json, raw as bp_raw} from 'body-parser';
-import path from 'path';
-import attachApplicationRoutes from './configure-express-routes';
+import express from "express";
+import { json as bp_json, raw as bp_raw } from "body-parser";
+import path from "path";
+import attachApplicationRoutes from "./configure-express-routes";
 import configureSessionWithRedis from "./configure-express-session";
 //@ts-ignore
 import { lookupService } from "./configure-services";
-import { RedisService } from './services/redis-service';
-import configureHandlebars from './configure-express-handlebars';
-import formatHttpException from './middleware/formatHttpException';
+import { RedisService } from "./services/redis-service";
+import configureHandlebars from "./configure-express-handlebars";
+import formatHttpException from "./middleware/formatHttpException";
+import { LogService } from "./services/log-service";
 
 export default async () => {
     // create app
     const app = express();
-    app.disable('x-powered-by');
+    app.disable("x-powered-by");
+
+    if (process.env.NODE_ENV !== "development") {
+        app.get("*", async (req, res, next) => {
+            if (req.protocol !== "https") {
+                const logService = (await lookupService(LogService.NAME)) as LogService;
+                const redirectUrl = `https://${req.headers.host}${req.originalUrl}`;
+                logService.info(`User is not using TLS - redirecting user to ${redirectUrl}`);
+                res.redirect(redirectUrl);
+            } else {
+                next();
+            }
+        });
+    }
 
     // configure app
-    app.use(express.static(path.join(__dirname, '..', 'public')));
+    app.use(express.static(path.join(__dirname, "..", "public")));
     app.use(bp_json());
     app.use(bp_raw()); // for smart.me protobuf
     configureHandlebars(app);
 
     // sessions
-    const redisService = await lookupService(RedisService.NAME) as RedisService;
+    const redisService = (await lookupService(RedisService.NAME)) as RedisService;
     app.use(configureSessionWithRedis(redisService.getClient()));
 
     // add routes to app
@@ -34,5 +48,5 @@ export default async () => {
     console.log(`Done configuring Express...`);
 
     // return the app
-    return app
-}
+    return app;
+};
