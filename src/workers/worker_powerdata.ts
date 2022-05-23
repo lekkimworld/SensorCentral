b6f0e49fd7f19d7a81b7abdab1bfed0ac39a24b0
Mikkel Flindt Heisterberg <lekkim@heisterberg.dk>
2022-05-23 15:47:07 +0200

    update powerdata-loader to also get data for tomorrow if run after 2pm

--- src/workers/worker_powerdata.ts
+++ src/workers/worker_powerdata.ts
@@ -9,7 +9,7 @@ import services from "../configure-services";
 import { LogService } from "../services/log-service";
 import { RedisService } from "../services/redis-service";
 import { StorageService } from "../services/storage-service";
-import moment from "moment-timezone";
+import moment, {Moment} from "moment-timezone";
 import { DataElement, Dataset } from "../resolvers/data";
 import { EventEmitter } from "stream";
 import constants from "../constants";
@@ -30,9 +30,8 @@ services.registerService(new DatabaseService());
 // get log service - we know it's ready
 const log = services.getService(LogService.NAME) as LogService;
 
-const doWork = async () => {
-    // create dataset
-    const m = moment();
+const fetchPowerdataForMoment = async (storage : StorageService, m : Moment) => {
+    log.info(`Getting powerdata for: ${m.format("YYYY-MM-DD")}`);
     const ds = {} as Dataset;
     ds.id = "power";
     ds.name = m.format("DD-MM-YYYY");
@@ -43,26 +42,47 @@ const doWork = async () => {
         date: m.format("YYYY-MM-DD"),
     };
 
+    // fetch data
+    const results = await new nordpool.Prices().hourly(opts);
+
+    // map data
+    ds.data = results.map((v: any) => {
+        let date = moment.utc(v.date);
+        let price = Number.parseFloat((v.value / 1000).toFixed(2)); // unit i MWh
+        let time = date.tz(constants.DEFAULTS.TIMEZONE).format("H:mm");
+        const elem = {} as DataElement;
+        elem.x = time;
+        elem.y = price;
+        return elem;
+    });
+
+    // cache
+    await storage.setPowerPriceData(m.format("YYYY-MM-DD"), ds.data);
+}
+
+const doWork = async () => {
+    // get storage service
+    const storage = (await services.lookupService(StorageService.NAME)) as StorageService;
+
+    // create moments to fetch for (allways current date but maybe also tomorrow)
+    const moments : Moment[] = [];
+    const momentToday = moment();
+    moments.push(momentToday);
+    if (momentToday.tz(constants.DEFAULTS.TIMEZONE).get("hour") > 14) {
+        // get for tomorrow
+        const momentTomorrow = moment().add(1, "day");
+        moments.push(momentTomorrow);
+    }
+
+    // create dataset and fetch data for each moment
     try {
-        const results = await new nordpool.Prices().hourly(opts);
+        log.info("Starting to load powerdata");
+        await Promise.all(moments.map(m => {
+            return fetchPowerdataForMoment(storage, m);
+        }))
 
-        // map data
-        ds.data = results.map((v: any) => {
-            let date = moment.utc(v.date);
-            let price = Number.parseFloat((v.value / 1000).toFixed(2)); // unit i MWh
-            let time = date.tz(constants.DEFAULTS.TIMEZONE).format("H:mm");
-            const elem = {} as DataElement;
-            elem.x = time;
-            elem.y = price;
-            return elem;
-        });
-
-        // cache
-        const storage = (await services.lookupService(StorageService.NAME)) as StorageService;
-        await storage.setPowerPriceData(m.format("YYYY-MM-DD"), ds.data);
-
-        // tell we are done
-        log.debug("Emitting finish-event to signal we are done loading powerdata");
+        log.info("Done loading powerdata");
+        log.debug("Emitting finish-event to signal we are done loading powerdata"); // tell we are done
         eventEmitter.emit("finish");
     } catch (err) {
         eventEmitter.emit("error", err);
