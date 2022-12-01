a025b16852ae338b0154d71bd13b8029b97b9113
Mikkel Flindt Heisterberg <lekkim@heisterberg.dk>
2022-12-01 23:39:47 +0100

    Add script to push docker image with versions

---
+++ push_docker.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+VERSION=`cat package.json| jq ".version" -r`
+IFS="." read -a VERSION_SPLIT <<< "$VERSION"
+echo "Version, full: $VERSION"
+echo "Version, major: ${VERSION_SPLIT[0]}"
+echo "Version, minor: ${VERSION_SPLIT[1]}"
+echo "Version, patch: ${VERSION_SPLIT[2]}"
+docker push lekkim/sensorcentral:$VERSION
+docker push lekkim/sensorcentral:${VERSION_SPLIT[0]}.${VERSION_SPLIT[1]}
+docker push lekkim/sensorcentral:${VERSION_SPLIT[0]}
