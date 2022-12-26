ecc96e2bb4156fd03693345294415e12a5a3205c
Mikkel Flindt Heisterberg <lekkim@heisterberg.dk>
2022-12-26 15:14:26 +0100

    Remove unused console.log

--- dev/js/widget-chart-favorite-binary-sensors.js
+++ dev/js/widget-chart-favorite-binary-sensors.js
@@ -1,6 +1,5 @@
 const baseWidget = require("./widget-chart-favorite-sensors-base");
 
 module.exports = (elem) => {
-    console.log(baseWidget);
     baseWidget(elem, "Favorite Binary Sensors", "binary");
 };
